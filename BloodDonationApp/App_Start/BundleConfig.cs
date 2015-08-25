8278cc662cf89b76d08e7db6a34e7f6d491a61f8
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-08-25 13:23:43 +0600

    Initial commit

---
+++ BloodDonationApp/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BloodDonationApp
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                      "~/Scripts/angular-*",
+                      "~/Scripts/errors.json"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular-ui").Include(
+                       "~/Scripts/ui-bootstrap-tpls.js",                       
+                         "~/Scripts/ui-bootstrap.js"
+                         ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/ui-bootstrap-csp.css",
+                      "~/Content/angular-csp.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
