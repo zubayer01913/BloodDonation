3d47075a3dec2474c45241019203dad3454f6203
newazhossain <newaz.duet@gmail.com>
2015-08-26 15:21:24 +0600

    Slider Added in home page

---
+++ BloodDonationApp/Scripts/AddContoller.js
@@ -0,0 +1,27 @@
+﻿angular.module('BloodDonationApp', ['ngAnimate', 'ui.bootstrap']);
+angular.module('BloodDonationApp').controller('SlideCtrl', function ($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.slides = [
+        {
+            image: '/Images/blood1.jpg',
+            text: 'Save a Life Give Blood'
+        },
+        {
+            image: '/Images/blood2.jpg',
+            text: 'Donate Blood'
+        },
+        {
+            image: '/Images/blood3.jpg',
+            text: 'Give the Gift of Blood'
+        },
+        {
+            image: '/Images/blood4.jpg',
+            text: 'Give the Gift of Life'
+        },
+        {
+            image: '/Images/blood5.jpg',
+            text: 'Help Save a Life Donate Blood'
+        }
+    ];
+});
