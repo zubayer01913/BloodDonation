864b5271df681ee86ff6606c7d85335ae2752f28
newazhossain <newaz.duet@gmail.com>
2015-08-26 12:45:38 +0600

    Bootstrap css added

--- BloodDonationApp/App_Start/BundleConfig.cs
+++ BloodDonationApp/App_Start/BundleConfig.cs
@@ -20,7 +20,11 @@ namespace BloodDonationApp
                          "~/Scripts/ui-bootstrap.js"
                          ));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
                       "~/Content/ui-bootstrap-csp.css",
                       "~/Content/angular-csp.css",
                       "~/Content/site.css"));
