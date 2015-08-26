6f5f51b964461403cd20a0609419ad9246eb60e3
mahbub-kuet <mrmanik.kuet@gmail.com>
2015-08-26 14:59:05 +0600

    SignUp page added.

---
+++ BloodDonationApp/Scripts/FormController.js
@@ -0,0 +1,11 @@
+﻿angular.module('myApp', []).controller('FormController', function ($scope) {
+    $scope.edit = true;
+    $scope.error = false;
+    $scope.incomplete = false;
+    $scope.options = [{ name: "A+", id: 1 }, { name: "A-", id: 2 }
+                      , { name: "B+", id: 1 }, { name: "B-", id: 2 },
+                        { name: "AB+", id: 1 }, { name: "AB-", id: 2 },
+    { name: "O+", id: 1 }, { name: "O-", id: 2 }];
+    $scope.selectedOption = $scope.options[1];
+
+});
